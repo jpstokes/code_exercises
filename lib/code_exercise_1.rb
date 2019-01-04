1cdde54d1e537f7605445307008d13060dcdb3e5
Jason Stokes <jpstokes@plecco.net>
2019-01-03 20:33:34 -0500

    Update code_exercise_1.rb

--- lib/code_exercise_1.rb
+++ lib/code_exercise_1.rb
@@ -1,5 +1,5 @@
 class CodeExercise1
-  RANGE = (-100000..100000).to_a
+  RANGE = (-1000000..1000000).to_a
   def smallest_absent_int(ary)
     num_array = (RANGE - ary).sort
     num_array.find { |i| i > 0 }
