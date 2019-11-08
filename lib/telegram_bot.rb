6bf561d0c7601c0e9d3fcf5955e9f8f0d78c29ce
Jason Stokes <jpstokes@plecco.net>
2019-11-08 17:22:35 -0500

    update bot

--- lib/telegram_bot.rb
+++ lib/telegram_bot.rb
@@ -37,31 +37,53 @@ module TelegramBot
           begin
             case message.text
             when '/testing'
-              bot.api.send_message(chat_id: message.chat.id, text: "#{message.from.first_name}, I'm happy to see you know the importance of testing.")
+              bot.api.send_message(
+                chat_id: message.chat.id,
+                text: "#{message.from.first_name}, I'm happy to see you know \
+                      the importance of testing.")
             when '/start'
-              bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+              bot.api.send_message(
+                chat_id: message.chat.id,
+                text: "Hello, #{message.from.first_name}")
             when '/stop'
-              bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+              bot.api.send_message(
+                chat_id: message.chat.id,
+                text: "Bye, #{message.from.first_name}")
             else
               user = User.new(
                 id: message.from.id,
                 first_name: message.from.first_name,
-                last_name: message.from.last_name
-              )
-              user_message_counts[user.id].nil? ? user_message_counts[user.id] = 1 : user_message_counts[user.id] += 1
-              bot.api.send_message(chat_id: message.chat.id, text: "Message sent successfully.")
+                last_name: message.from.last_name)
+
+              if user_message_counts[user.id].nil?
+                user_message_counts[user.id] = 1
+              else
+                user_message_counts[user.id] += 1
+              end
+
+              bot.api.send_message(
+                chat_id: message.chat.id,
+                text: "Message sent successfully.")
+
               if user_message_counts[user.id] == 3
                 # award user with bitcoin
                 result = @client.transfer_funds_to(user)
-                raise Exception.new('There was a problem transferring funds.') unless result
-                bot.api.send_message(chat_id: message.chat.id, text: "Congrats, #{user.first_name}, you've earned $1 worth of bitcoin!")
+                unless result
+                  raise Exception.new('There was a problem transferring funds.')
+                end
+                bot.api.send_message(
+                  chat_id: message.chat.id,
+                  text: "Congrats, #{user.first_name}, you've earned $1 worth \
+                        of bitcoin!")
                 bot.api.send_message(
                   chat_id: message.chat.id,
                   text: current_balance_display(result[:user_address])
                 )
                 user_message_counts.delete(user.id)
               else
-                bot.api.send_message(chat_id: message.chat.id, text: "Message tally: #{user_message_counts[user.id]}")
+                bot.api.send_message(
+                  chat_id: message.chat.id,
+                  text: "Message tally: #{user_message_counts[user.id]}")
               end
             end
           rescue Exception => e
@@ -74,21 +96,33 @@ module TelegramBot
     private
 
       def current_balance_display(address)
-        "Your current balance: #{@client.current_balance(address, :btc)} BTC / $#{@client.current_balance(address, :usd)} USD"
+        "Your current balance: #{@client.current_balance(address, :btc)} BTC \
+        / $#{@client.current_balance(address, :usd)} USD"
       end
   end
 
   class BlockIoClient
     def initialize
-      BlockIo.set_options(api_key: ENV['BITCOIN_API_KEY'], pin: ENV['BITCOIN_SECRET_PIN'], version: 2)
+      BlockIo.set_options(
+        api_key: ENV['BITCOIN_API_KEY'],
+        pin: ENV['BITCOIN_SECRET_PIN'],
+        version: 2)
     end
 
     def transfer_funds_to(user)
       from_address = get_or_create_address_by_label(DEFAULT_LABEL)
       to_address = get_or_create_address_by_label(user.block_io_label)
       transfer_amount = calculate_1_usd_of_bitcoin
-      raise Exception.new('Transfer amount exceeds current balance') if transfer_amount > current_balance
-      BlockIo.withdraw_from_addresses(amounts: transfer_amount, from_addresses: from_address, to_addresses: to_address)
+
+      if transfer_amount > current_balance
+        raise Exception.new('Transfer amount exceeds current balance')
+      end
+
+      BlockIo.withdraw_from_addresses(
+        amounts: transfer_amount,
+        from_addresses: from_address,
+        to_addresses: to_address)
+
       { user_address: to_address }
     end
 
@@ -99,7 +133,8 @@ module TelegramBot
         BlockIo.get_address_balance addresses: addresses
       end
       if format == :usd
-        ('%.2f' % (response['data']['available_balance'].to_f * usd_value).round(2)).to_f
+        ('%.2f' % (response['data']['available_balance'].to_f * usd_value)
+          .round(2)).to_f
       else
         response['data']['available_balance'].to_f
       end
@@ -120,7 +155,8 @@ module TelegramBot
       end
 
       def usd_value
-        # hide following line since BlockIo doesn't provide value for test accounts
+        # hide following line since BlockIo doesn't provide value for test
+        # accounts
         # @usd_value ||= BlockIo.get_current_price(price_base: 'USD')
         3834.655
       end
